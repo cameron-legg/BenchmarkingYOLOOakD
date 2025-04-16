c3967b966bb1a350bc22e3ba87d292d77a6a68e9
Cameron Legg <cameron.legg@gmail.com>
2025-04-16 13:21:25 -0600

    added benchmarks

--- HelpfulScripts/pullresultfromorin.sh
+++ HelpfulScripts/pullresultfromorin.sh
@@ -1,3 +1,3 @@
-scp -r clegg@orin:~/project/videos/out/* ../videos/out/
+# scp -r clegg@orin:~/project/videos/out/* ../videos/out/
 scp -r clegg@orin:~/project/benchmark_results ../.
 # scp -r clegg@orin:~/project/*.txt ../.
