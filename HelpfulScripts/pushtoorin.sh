b4e10635459c24fa2da5cda122f0246ffd973f2a
Cameron Legg <cameron.legg@gmail.com>
2025-04-15 19:52:54 -0600

    orin

--- HelpfulScripts/pushtoorin.sh
+++ HelpfulScripts/pushtoorin.sh
@@ -6,6 +6,18 @@ tar -cf archive.tar ../oakd_models/fall_detection_6shaves/falldetectionmodel_ope
     ../oakd_models/fall_detection_8shaves/best.json \
     ../oakd_models/fall_detection_16shaves/best_openvino_2022.1_16shave.blob \
     ../oakd_models/fall_detection_16shaves/best.json \
+    ../oakd_models/fall_detection_1shave/best_openvino_2022.1_1shave.blob \
+    ../oakd_models/fall_detection_1shave/best.json \
+    ../oakd_models/fall_detection_2shaves/best_openvino_2022.1_2shave.blob \
+    ../oakd_models/fall_detection_2shaves/best.json \
+    ../oakd_models/fall_detection_4shaves/best_openvino_2022.1_4shave.blob \
+    ../oakd_models/fall_detection_4shaves/best.json \
+    ../oakd_models/fall_detection_10shaves/best_openvino_2022.1_10shave.blob \
+    ../oakd_models/fall_detection_10shaves/best.json \
+    ../oakd_models/fall_detection_12shaves/best_openvino_2022.1_12shave.blob \
+    ../oakd_models/fall_detection_12shaves/best.json \
+    ../oakd_models/fall_detection_14shaves/best_openvino_2022.1_14shave.blob \
+    ../oakd_models/fall_detection_14shaves/best.json \
     ../pt_models/fall_detection/weights/best.pt \
     ../videos/in/benchmarkvid.mp4 \
     ../videos/out/placeholder \
@@ -15,10 +27,6 @@ tar -cf archive.tar ../oakd_models/fall_detection_6shaves/falldetectionmodel_ope
     ../oakd_benchmarked.py \
     ../tmp/placeholder \
     ../Master_Benchmark.py
-    # ../fromfile_cpu.py \
-    # ../fromfile.py \
-    # ../fromstream.py \
-    # ../fromfile_gpu.py \
 
 
 scp archive.tar clegg@orin:~/project
